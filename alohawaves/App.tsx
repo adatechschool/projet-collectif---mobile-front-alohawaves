224469e49c8766489f89238286018ad7dc58a3cd
Fanny <fanny.olengabinene@laposte.net>
2022-09-05 14:30:14 +0200

    front spot list

--- alohawaves/App.tsx
+++ alohawaves/App.tsx
@@ -1,37 +1,36 @@
 // Porte d'entrée de l'App
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View, Image } from 'react-native';
+import { StyleSheet, Text, View, Image, ImageBackground } from 'react-native';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <StatusBar style="auto" />
+    <StatusBar style="auto" />
       {/* <View>
         <Text>Welcome Alohawaves !</Text>
   </View> */}
 
-      <View style={styles.imagebox}>
-        <Image style={styles.imagestyle}
-          source={{
-            uri:'https://sportihome.com/uploads/spots/59a70f35b27eb115986b6247/large/1504121018914.jpg'}}/>
+      <View >
+          <Text style={styles.titlelist}>SPOTS</Text>
       </View>
 
       <View style={styles.descriptionbox}>
-          <Text style={styles.titlestyle}>Plage d'Hossegor</Text>
+            <Text style={styles.titlestyle}>Plage d'Hossegor</Text>
 
-          <Text style={styles.locationstyle}>Hossegor, France</Text>
+            <Text style={styles.locationstyle}>Hossegor, France</Text>
 
-          <View style={styles.detailstyle}>
-            <Text style={styles.seasonstyle}>Haute Saison</Text>
-            <Image source={require('./src/assets/wave.png')} style={styles.difficultystyle}/>
-            <Text style={styles.beachstyle}> </Text>
-          </View>
-       
-          <Text style={styles.descriptionstyle}>Aut exercitationem obcaecati vel tempore galisum eum aperiam fugiat a repellendus explicabo et adipisci tempore quo architecto accusamus? Et sapiente consequuntur sed consequatur vitae vel ipsum blanditiis est expedita reprehenderit non porro doloremque assumenda eveniet.</Text>
+            
+
+      <View style={styles.imagebox}>
+                 <ImageBackground source={{
+              uri:'https://sportihome.com/uploads/spots/59a70f35b27eb115986b6247/large/1504121018914.jpg'}} style={styles.detailstyle}>
+                  <Image source={require('./src/assets/wave.png')} style={styles.difficultystyle}/>
+                </ImageBackground>
+
+            </View>
+        </View>
 
       </View>
-
-    </View>
   );
 }
 
@@ -45,20 +44,21 @@ const styles = StyleSheet.create({
     width: '100%',
   },
   imagestyle: {
-    height: '100%',
+    height: '35%',
   }, 
   descriptionbox: {
     flex: 2,
     width: '100%',
     backgroundColor: '#fcfcfa',
-    padding: 30, 
   },
   titlestyle: {
     fontWeight: 'bold',
-    fontSize: 25
+    fontSize: 25,
+    paddingLeft: 20,
   },
   locationstyle: {
     paddingBottom: 10,
+    paddingLeft: 20,
     color: '#535254',
   },
   detailstyle: {
@@ -66,18 +66,16 @@ const styles = StyleSheet.create({
     justifyContent: 'space-between',
     paddingBottom: 10,
   },
-  seasonstyle: {
-
-  },
   difficultystyle: {
     height: 25,
     width: 25,
   },
-  beachstyle: {
-
-  },
-  descriptionstyle: {
-    lineHeight: 23,
-    color: '#535254',
+  titlelist: {
+    padding: 50,
+    textAlign: 'center',
+    fontSize: 30,
+    fontWeight: 'bold',
+    
   }
+
 });
---
+++ alohawaves/src/screens/spot.tsx
@@ -0,0 +1,83 @@
+// Porte d'entrée de l'App
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View, Image } from 'react-native';
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <StatusBar style="auto" />
+      {/* <View>
+        <Text>Welcome Alohawaves !</Text>
+  </View> */}
+
+      <View style={styles.imagebox}>
+        <Image style={styles.imagestyle}
+          source={{
+            uri:'https://sportihome.com/uploads/spots/59a70f35b27eb115986b6247/large/1504121018914.jpg'}}/>
+      </View>
+
+      <View style={styles.descriptionbox}>
+          <Text style={styles.titlestyle}>Plage d'Hossegor</Text>
+
+          <Text style={styles.locationstyle}>Hossegor, France</Text>
+
+          <View style={styles.detailstyle}>
+            <Text style={styles.seasonstyle}>Haute Saison</Text>
+            <Image source={require('./src/assets/wave.png')} style={styles.difficultystyle}/>
+            <Text style={styles.beachstyle}> </Text>
+          </View>
+       
+          <Text style={styles.descriptionstyle}>Aut exercitationem obcaecati vel tempore galisum eum aperiam fugiat a repellendus explicabo et adipisci tempore quo architecto accusamus? Et sapiente consequuntur sed consequatur vitae vel ipsum blanditiis est expedita reprehenderit non porro doloremque assumenda eveniet.</Text>
+
+      </View>
+
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+  },
+  imagebox: {
+    flex: 1,
+    width: '100%',
+  },
+  imagestyle: {
+    height: '100%',
+  }, 
+  descriptionbox: {
+    flex: 2,
+    width: '100%',
+    backgroundColor: '#fcfcfa',
+    padding: 30, 
+  },
+  titlestyle: {
+    fontWeight: 'bold',
+    fontSize: 25
+  },
+  locationstyle: {
+    paddingBottom: 10,
+    color: '#535254',
+  },
+  detailstyle: {
+    flexDirection: 'column',
+    justifyContent: 'space-between',
+    paddingBottom: 10,
+  },
+  seasonstyle: {
+
+  },
+  difficultystyle: {
+    height: 25,
+    width: 25,
+  },
+  beachstyle: {
+
+  },
+  descriptionstyle: {
+    lineHeight: 23,
+    color: '#535254',
+  }
+});
